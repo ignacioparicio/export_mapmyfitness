448717db86771da34220e93a4642430442de7b69
Ignacio Paricio <ignacio.paricio@outlook.com>
2021-05-01 18:45:30 +0200

    Add feature to extract TCX from individual mapmyfitness trackings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
 
-browser = webdriver.Chrome()
-browser.maximize_window()
+workout_data = pd.read_csv("../data/user118862045_workout_history.csv")
+workout_links = workout_data["Link"].to_list()
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get(workout_links[0])
+input(
+    "Press enter once authenticated on mapmyfitness.com website and the "
+    "downloads folder is changed to the desired location..."
+)
+
+
+for link in workout_links:
+    time.sleep(2)
+
+    driver.get(link)
+
+    # TODO: implement more neatly with `WebDriverWait`
+    time.sleep(10)
+
+    # Buttons are given dynamic jss classes, but the button that leads to exports is
+    # always the first one
+    buttons = driver.find_elements_by_xpath("//button")
+    buttons[0].click()
+
+    driver.find_element_by_xpath("//*[contains(text(), 'Download as TCX')]").click()
