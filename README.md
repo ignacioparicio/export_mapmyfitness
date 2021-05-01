bc830d3bc5648efae70fda49a37025a29e967105
Ignacio Paricio <ignacio.paricio@outlook.com>
2021-05-01 17:33:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+A script to automatically export fitness workouts from Under Armour's map my fitness,
+in `TCX` format.
+
+# Requirements
+* 
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+browser = webdriver.Chrome()
+browser.maximize_window()
